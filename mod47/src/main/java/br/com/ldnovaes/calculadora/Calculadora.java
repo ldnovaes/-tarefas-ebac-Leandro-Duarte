9771296195ba4e6112c7949f6f9eedaa44f64ee6
Leandro Duarte <leandroduarte2012@hotmail.com>
2023-12-04 17:41:27 -0300

    modificado javadoc em mod47

--- mod47/src/main/java/br/com/ldnovaes/calculadora/Calculadora.java
+++ mod47/src/main/java/br/com/ldnovaes/calculadora/Calculadora.java
@@ -20,8 +20,9 @@ public class Calculadora {
     /**
      *
      * Método que realiza a subtração de dois inteiros
-     * @param a
-     * @param b
+     * Tome cuidado com a posição dos números passados em parâmetro. O @param a é referente ao primeiro número de uma subtração, enquanto que o @param b é o segundo
+     * @param a primeiro parâmetro
+     * @param b segundo parâmetro
      * @return
      */
     public int subtrair(int a, int b) {
@@ -42,6 +43,7 @@ public class Calculadora {
     /**
      * 
      * Método que realiza a divisão de dois números inteiros
+     * Tome cuidado com a posição dos números passados em parâmetro. O @param a é referente ao dividendo, enquanto que o @param b é o divisor
      * @param a
      * @param b
      * @throws ArithmeticException se o divisor for zero
